00c8bb83b39aa207e7a8fb6c520d42ba7322efc9
Aspyn <jeongfree25@gmail.com>
2024-10-09 13:20:26 -0700

    finish add feature part 1

--- To_do_list_App.py
+++ To_do_list_App.py
@@ -6,10 +6,14 @@ App_list = ["To-Do List Application",
         "4. Exit"]
 
 def app_function(num):
-    agenda_list = [] # list of tasks
+    agenda_list = [] # list of tasks, initialize an empty list
     if num == 1:
         # add
-        print("add agenda")
+        add_task = input("Enter the task: ")
+        agenda_list.append(add_task)
+        print(f"\'{add_task}\' has been added to the list.")
+        print()
+
     elif num == 2:
         # remove
         print("remove agenda")
@@ -18,6 +22,7 @@ def app_function(num):
         print("view agenda")
 
 while True :
+    # show the application list
     for i in App_list:
         print(i)
     try :
