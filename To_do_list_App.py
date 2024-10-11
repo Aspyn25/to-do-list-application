c0865b99dd8acd78faf30a3a441e7f7cb2d5b0ba
Aspyn <jeongfree25@gmail.com>
2024-10-10 21:53:38 -0700

    feature/add part2 - adding priority and deadline

--- To_do_list_App.py
+++ To_do_list_App.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 # add function
 App_list = ["To-Do List Application",
         "1. Add Task",
@@ -5,14 +7,16 @@ App_list = ["To-Do List Application",
         "3. View Tasks",
         "4. Exit"]
 
+# list of tasks, initialize an empty list
+agenda_list = []
+#deadline = ""
+
+
 def app_function(num):
-    agenda_list = [] # list of tasks, initialize an empty list
+
     if num == 1:
-        # add
-        add_task = input("Enter the task: ")
-        agenda_list.append(add_task)
-        print(f"\'{add_task}\' has been added to the list.")
-        print()
+        # add def
+        adding()
 
     elif num == 2:
         # remove
@@ -20,6 +24,33 @@ def app_function(num):
     else:
         #view
         print("view agenda")
+
+def adding():
+    # adding task
+    add_task = input("Enter the task: ")
+    # adding priority
+    while True:
+        priority = input("Enter the priority (high, medium, low): ")
+        if priority == 'high' or priority == 'medium' or priority == 'low':
+            break
+        else:
+            print("you should choose one of them (high, medium, low).")
+    # adding deadline
+    while True:
+        try:
+            deadline = input("Enter the deadline (YYYY-MM-DD): ")
+            # date() : only pick date
+            date_object = datetime.strptime(deadline, "%Y-%m-%d").date()
+            date_object = str(date_object).split("-")
+            break
+        except ValueError:
+            print("You should follow this format using number(YYYY-MM-DD).")
+
+    agenda_list.append([add_task, priority, date_object])
+    print(f"\'{add_task}\' with priority \'{priority}\' and deadline \'{date_object[0]}-{date_object[1]}-{date_object[2]}\' has been added to the list.")
+    print(agenda_list)
+
+
 
 while True :
     # show the application list
@@ -37,3 +68,4 @@ while True :
         print("you put the wrong number. you need to put the number(1 to 4).")
     else:
         app_function(num)
+
